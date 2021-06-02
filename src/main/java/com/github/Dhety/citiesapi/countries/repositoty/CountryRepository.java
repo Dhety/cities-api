0dcba92305bfa050810aa0e01cb047a5c6b4f9ae
root <root@Julian-ROGSTRIX.localdomain>
2021-06-02 12:38:44 -0300

    Primeiro Commit

---
+++ src/main/java/com/github/Dhety/citiesapi/countries/repositoty/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.github.Dhety.citiesapi.countries.repositoty;
+
+import com.github.Dhety.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+}
